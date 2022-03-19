68bd247e6bda78aa90891f8f6028b53ffa8e115c
tgtran <tg.trann@gmail.com>
2022-03-19 16:40:59 -0400

    add event listener to button to create new tasks

--- todo.js
+++ todo.js
@@ -1,10 +1,20 @@
 const items = document.querySelectorAll('li');
 const button = document.querySelector('button');
+const ul = document.querySelector('ul');
 
+//create a list element when button is clicked
+button.addEventListener('click', () => {
+    const li = document.createElement('li');
+    li.textContent = 'something new';
+    ul.prepend(li);
+});
+
+//iterate through each for loop and remove each task when clicked
 items.forEach(item => {
     item.addEventListener('click', e => {
         e.target.remove();
     });
 });
+
 
 
